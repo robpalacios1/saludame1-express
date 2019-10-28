152cd277e56a468f1e66a18d1c09bab53a04cc9a
Mario Vizcaino <mario.vizcainos@gmail.com>
2019-10-28 14:43:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,14 @@ const express = require('express');
 const app = express();
 
     app.get('/', (req, res) => {
-
-        if(req.query.nombre){
-            res.send(`<h1>Hola ${req.query.nombre}!</h1>`)
-
-        }else{
-
-            res.send(`<h1>Hola desconocido!</h1>`)
-
-        }
+        
+        let name = req.query.nombre ?
+          req.query.nombre :
+          'desconocido';
+        
+        res.send(`<h1>Hola ${name}!</h1>`)
 
     });
 
 
 app.listen(3000, () => console.log('Listening on port 3000'));
-
-
-
-
-
